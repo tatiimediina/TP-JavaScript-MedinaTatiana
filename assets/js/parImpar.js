cfdcf8f0d15630adc007e7fc3a40b12ef8f08739
Tatiana Medina <tatymedina284@gmail.com>
2024-05-05 10:11:35 -0300

    Trabajo finalizado

--- assets/js/parImpar.js
+++ assets/js/parImpar.js
@@ -1,7 +1,7 @@
 let num = parseInt(prompt("Ingrese un número:"));
 
 if (isNaN(num)) {
-    console.log("Lo ingresado no es válido")
+    console.log("Lo ingresado no es válido");
 } else if (num % 2 === 0){
     console.log("El número es par");
 } else if (num % 2 !== 0) {
