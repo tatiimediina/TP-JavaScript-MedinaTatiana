711d01984c620e1200c9fe5a45ecb6d02c4488b7
Tatiana Medina <tatymedina284@gmail.com>
2024-05-05 10:03:02 -0300

    Solucioné un error que encontré en el ejercicio dos

--- assets/js/parImpar.js
+++ assets/js/parImpar.js
@@ -1,9 +1,9 @@
 let num = parseInt(prompt("Ingrese un número:"));
 
-if (num % 2 === 0){
+if (isNaN(num)) {
+    console.log("Lo ingresado no es válido")
+} else if (num % 2 === 0){
     console.log("El número es par");
 } else if (num % 2 !== 0) {
     console.log("El número es impar");
-} else {
-    console.log("Lo ingresado no es válido");
-}
+} 
